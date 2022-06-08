ed91d5dc9020f736e2fb6549351102d20e9d7fe7
shingha <shingha1124@gmail.com>
2022-06-08 10:59:19 +0900

    폴더 정리

--- airbnb/airbnb/Sources/Present/Common/ViewController/CheckInOut/CheckInOutViewModelProtocol.swift
+++ airbnb/airbnb/Sources/Present/Common/ViewController/CheckInOut/CheckInOutViewModelProtocol.swift
@@ -9,20 +9,4 @@ import Foundation
 import RxDataSources
 import RxRelay
 
-protocol CheckInOutViewModelAction {
-    var viewDidLoad: PublishRelay<Void> { get }
-    var tappedRemoveButton: PublishRelay<Void> { get }
-}
-
-protocol CheckInOutViewModelState {
-    var showCalender: PublishRelay<[SectionModel<String, CalenderCellViewModel>]> { get }
-    var selectedDates: PublishRelay<CheckInOut> { get }
-}
-
-protocol CheckInOutViewModelBinding {
-    func action() -> CheckInOutViewModelAction
-    func state() -> CheckInOutViewModelState
-}
-
-typealias CheckInOutViewModelProtocol = CheckInOutViewModelBinding
 typealias CheckInOut = (checkIn: Date?, checkOut: Date?)
